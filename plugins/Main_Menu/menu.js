6102252bb705e15b7f7d14413bcddf4f065bb41b
MaouDabi <erkipandial@gmail.com>
2025-07-13 20:49:16 +0700

    Update Dabi-Ai
    Update 1.3.2

--- plugins/Main_Menu/menu.js
+++ plugins/Main_Menu/menu.js
@@ -1,5 +1,6 @@
 const { generateWAMessageFromContent } = require('@whiskeysockets/baileys');
 const config = require('../../toolkit/set/config.json');
+const db = require('../../toolkit/db/database.json');
 
 const more = String.fromCharCode(8206);
 const readmore = more.repeat(4001);
@@ -20,13 +21,20 @@ module.exports = {
   }) => {
     const { chatId, senderId, isGroup, pushName } = chatInfo;
     const requestedCategory = args.length ? args.join(' ').toLowerCase() : null;
-    const menuText = getMenuText(pushName, requestedCategory, prefix);
+    const totalUser = Object.keys(db.Private || {}).length;
+    const menuText = getMenuText(pushName, requestedCategory, prefix, totalUser);
 
     await conn.sendMessage(chatId, {
-      image: thumb,
-      caption: menuText,
+      text: menuText,
       contextInfo: {
-        forwardingScore: 0,
+        externalAdReply: {
+          title: botFullName,
+          body: `Ini Adalah Menu ${botName}`,
+          thumbnailUrl: thumbnail,
+          mediaType: 1,
+          renderLargerThumbnail: true,
+        },
+        forwardingScore: 1,
         isForwarded: true,
         forwardedNewsletterMessageInfo: {
           newsletterJid: '120363310100263711@newsletter'
@@ -36,17 +44,17 @@ module.exports = {
   }
 };
 
-function getMenuText(sender, requestedCategory, prefix) {
+function getMenuText(sender, requestedCategory, prefix, totalUser) {
   let menuText = `Halo *${sender}*, Saya adalah asisten virtual yang siap membantu.\n`;
-  menuText += `Gunakan perintah di bawah untuk berinteraksi dengan saya.\n`;
-  menuText += `> ⚠ Note:\n> Bot ini masih dalam tahap pengembangan,\n> jadi gunakan dengan bijak\n\n`;
+  menuText += `Gunakan perintah di bawah untuk berinteraksi dengan saya.\n\n`;
 
   menuText += `${head} ${Obrack} *Info ${botName}* ${Cbrack}\n`;
   menuText += `${side} ${btn} Bot Name: ${botFullName}\n`;
   menuText += `${side} ${btn} Owner: ${ownerName}\n`;
   menuText += `${side} ${btn} Type: ${type}\n`;
   menuText += `${side} ${btn} Tutorial: .help\n`;
-  menuText += `${side} ${btn} Total: ${Object.keys(global.plugins).length} Cmd\n`;
+  menuText += `${side} ${btn} Total Cmd: ${Object.keys(global.plugins).length} Cmd\n`;
+  menuText += `${side} ${btn} Total User: ${totalUser}\n`
   menuText += `${side} ${btn} Versi: ${version}\n`;
   menuText += `${side} ${btn} Baileys: ${baileys}\n`;
   menuText += `${foot}${garis}\n\n`;
